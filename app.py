90b2b8c87f6f6bc251cff2fd7dcefca0c66df7c6
TheLighthouse36 <38276450+TheLighthouse36@users.noreply.github.com>
2022-06-03 01:53:34 -0700

    I added the proof of concept projects display and routing. This is mainly showing the overall structure. it will get more refined over time

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import MySQLdb.cursors
 
 app = Flask(__name__)
  
-app.config['MYSQL_HOST'] = 'ls-0e6dd62320aa7f6d260d0d5825892f815269cf28.cmprcwekrhkn.us-west-2.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'dbmasteruser'
-app.config['MYSQL_PASSWORD'] = 'vET[O|{UeZ&9=FBqn9`x)9G3~fQtSC|^'
-app.config['MYSQL_DB'] = 'BugAccountant'
+#app.config['MYSQL_HOST'] = 'ls-0e6dd62320aa7f6d260d0d5825892f815269cf28.cmprcwekrhkn.us-west-2.rds.amazonaws.com'
+#app.config['MYSQL_USER'] = 'dbmasteruser'
+#app.config['MYSQL_PASSWORD'] = 'vET[O|{UeZ&9=FBqn9`x)9G3~fQtSC|^'
+#app.config['MYSQL_DB'] = 'BugAccountant'
  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Dragonfruit#4'
+app.config['MYSQL_DB'] = 'BugAccountant'
+
 mysql = MySQL(app)
  
 @app.route('/')
@@ -22,6 +27,30 @@ def data_test():
     
     return render_template('GlobalUsersTable.html', data=data)
 
+@app.route('/projects')
+def project_test():
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    cursor.execute("select * from Projects")
+
+    data = cursor.fetchall()
+
+    return render_template('GlobalProjectsTable.html', data=data)
+
+@app.route('/projects/<project_entry>')
+def project_entry(project_entry):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    cursor.execute("""  select Projects.ProjectID, ProjectName, UserID, ProjectRole 
+                        from Projects 
+                        join ProjectUsers 
+                        on Projects.ProjectID = ProjectUsers.ProjectID
+                        Where Projects.ProjectName = \"""" + project_entry + "\"")
+
+    data = cursor.fetchall()
+
+    return render_template('ProjectEntry.html', data=data)
  
 app.run(host='localhost', port=5000)
 
